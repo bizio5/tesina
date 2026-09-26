e975142176f5ab8c65085418d595fea306ee5d46
bizio <pavonifab@gmail.com>
2026-09-26 14:48:56 +0200

    fatto la base per selezionare l opzione,mancano le funzioni vere e proprio

--- client.c
+++ client.c
@@ -13,8 +13,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define port 9998
+#define port 9997
 #define buffersize 1024
+
+struct messaggio{
+        char destinatario[32];
+        char oggetto[64];
+        char testo[1024];
+};
+
+void letturamessaggi(int client_fd,int scelta){
+
+    printf("opzione1\n");
+    send(client_fd,&scelta,sizeof(int),0);
+
+}
+
+void spedizione(int client_fd,int scelta ){
+
+    printf("opzione2\n");
+    send(client_fd,&scelta,sizeof(int),0);
+    
+    struct messaggio msg;
+    printf("inserisci destinatario:");
+    fgets(msg.destinatario,sizeof(msg.destinatario),stdin);
+
+    printf("inserisci oggetto:");
+    fgets(msg.oggetto,sizeof(msg.oggetto),stdin);
+
+    printf("inserisci testo:");
+    fgets(msg.testo,sizeof(msg.testo),stdin);
+
+
+    send(client_fd,&msg,sizeof(struct messaggio),0);
+
+
+}
+
+void cancellazione(int client_fd,int scelta){
+    printf("opzione3\n");
+    send(client_fd,&scelta,sizeof(int),0);
+}
+
+
+
+
+
+
+
 
 
 int main(int argc, char **argv){
@@ -42,12 +88,39 @@ int main(int argc, char **argv){
         exit(EXIT_FAILURE);
     }
 
-    char messaggio[buffersize] = "Ciao server, sono il client!";
-    send(client_fd, messaggio, strlen(messaggio), 0);
+     while(1){
+        printf("inserire cosa si vuole fare digitando il numero associato all operaizone:\n");
+        printf("1)lettura di tutti i messaggi inviati\n");
+        printf("2)spedire un messaggio a un utente\n");
+        printf("3)cancellazioni di tutti i messaggi ricevuti\n");
+        printf("4)chiudere connessione\n");
+        
+        char richiesta[3];
 
-    close(client_fd);
+        fgets(richiesta,sizeof(richiesta),stdin);
+        int scelta = atoi(richiesta);
 
-
-
-    
+        printf("%d\n",scelta);
+        
+        switch (scelta) {
+            case 1:
+                letturamessaggi(client_fd,scelta);
+                break;
+            case 2:
+                spedizione(client_fd,scelta);
+                break;
+            case 3:
+                cancellazione(client_fd,scelta);
+                break;
+            case 4:
+                close(client_fd);
+                printf("connesione chiusa\n");
+                break;
+            default:
+                printf("inserire numero valido\n");
+        }
+        if (scelta == 4)
+            break;
+    }
 }
+
--- server.c
+++ server.c
@@ -13,12 +13,85 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define port 9998
+#define port 9997
 #define buffersize 1024
 
+int connessione = 0;
+
+struct param{
+    int socket;
+    char *socket_name;
+};
+
+struct messaggio{
+        char destinatario[32];
+        char oggetto[64];
+        char testo[1024];
+};
+
+void *func_thread(void *arg){
+
+    struct param * param_thread = (struct param *) arg;
+    int fd = open(param_thread -> socket_name,O_CREAT | O_RDWR ,0666);
+
+    while(1){
+        int scelta;
+        printf("thread %d\n",connessione);
+        ssize_t byte = recv(param_thread -> socket,&scelta,sizeof(int),0);
+
+        if (byte <= 0) {
+            printf("Client disconnesso o errore.\n");
+            break; // ESCE DAL CICLO: Evita il loop infinito!
+        }
+
+        switch(scelta) {
+            //valutare se fare funzioni per i vari case
+            case 1:
+                printf("Richiesta lettura messaggi...\n");
+                break;
+                
+            case 2: {
+                printf("Ricezione nuovo messaggio in corso...\n");
+                
+                struct messaggio msg_ricevuto;
+                ssize_t byte_msg = recv(param_thread->socket, &msg_ricevuto, sizeof(struct messaggio), 0);
+                
+                if (byte_msg > 0) {
+                    printf("Destinatario: %s", msg_ricevuto.destinatario);
+                    printf("Oggetto: %s", msg_ricevuto.oggetto);
+                    printf("Testo: %s", msg_ricevuto.testo);
+                }
+                break;
+            }
+            
+            case 3:
+                printf("Richiesta cancellazione...\n");
+                break;
+                
+            case 4:
+                printf("Il client ha chiesto di chiudere.\n");
+                close(param_thread->socket);
+                break;
+                
+            default:
+                printf("Comando non valido ricevuto.\n");
+                break;
+        }
+        if (scelta == 4) {
+            break;
+        }
+       
+    }
+    close(fd);
+    close(param_thread->socket);
+    free(param_thread);
+    return NULL;
+
+}
+
 int main(int argc, char **argv){
-    int connessione = 0;
-    int new_socket, server_fd;
+    
+    int server_fd;
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Errore nella creazione del socket");
         exit(EXIT_FAILURE);
@@ -30,37 +103,40 @@ int main(int argc, char **argv){
     //htons serve per convertire il numero di porta in network byte order
     servAddr.sin_port = htons(port);
     servAddr.sin_addr.s_addr = INADDR_ANY;
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("Errore nella setsockopt");
+        exit(EXIT_FAILURE);
+    }
 
     if (bind(server_fd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
         perror("Errore nel bind");
         exit(EXIT_FAILURE);
     }
-     if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0) {
             perror("Errore nella listen");
             exit(EXIT_FAILURE);
         }
+    
     while(1){
 
         struct sockaddr_in clientAddr;
         socklen_t addrlen = sizeof(clientAddr);
+        pthread_t thread;
+        struct param *param_main = malloc(sizeof(struct param));
 
-        if ( (new_socket = accept(server_fd, (struct sockaddr *)&clientAddr, &addrlen)) < 0) {
+
+        if ( (param_main -> socket = accept(server_fd, (struct sockaddr *)&clientAddr, &addrlen)) < 0) {
             perror("Errore nell'accept");
             exit(EXIT_FAILURE);
         }
         else{
             connessione++;
             printf("Numero di connessioni: %d\n", connessione);
-            
 
+            param_main -> socket_name = inet_ntoa(clientAddr.sin_addr);
+
+            pthread_create(&thread,NULL,func_thread, param_main);
         }
-        
-        char messaggio[buffersize];
-        recv(new_socket, messaggio, buffersize, 0);
-        printf("Messaggio ricevuto dal client: %s\n", messaggio);
-
-        close(new_socket);
-
     }
-
 }
